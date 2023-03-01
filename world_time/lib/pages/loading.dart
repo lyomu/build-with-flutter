2b1b87c9a777c1075e007afc27f4b86b6d77edde
lyomu <gmnyomu@gmail.com>
2023-03-01 11:51:07 +0300

    World time App

---
+++ world_time/lib/pages/loading.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:http/http.dart';
+import 'dart:convert';
+
+class Loading extends StatefulWidget {
+  const Loading({Key? key}) : super(key: key);
+
+  @override
+  State<Loading> createState() => _LoadingState();
+}
+
+class _LoadingState extends State<Loading> {
+  void getTime() async {
+    //make request
+    Response response = await get('http://worldtimeapi.org/api/timezone/Africa/Nairobi');
+    Map data
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    getTime();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('Loading Page'),
+    );
+  }
+}
